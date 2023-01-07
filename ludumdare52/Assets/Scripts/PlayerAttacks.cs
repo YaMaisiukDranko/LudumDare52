165eb6fb783458c7fb903f050cae6ebc5914d357
Yaroslav <yarikmeisan@yandex.com>
2023-01-07 17:55:40 +0300

    Setting up player attack and damage

--- ludumdare52/Assets/Scripts/PlayerAttacks.cs
+++ ludumdare52/Assets/Scripts/PlayerAttacks.cs
@@ -1,8 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
-    
+    public float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+    private void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Collider2D[] enemiesToDamage =
+                    Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().health -= damage;
+                }
+            }
+
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
 }
