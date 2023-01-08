51e123304cf91fc8e68f0b48e72f359914a04db1
Yaroslav <yarikmeisan@yandex.com>
2023-01-08 16:26:46 +0300

    Setup get enemy

--- ludumdare52/Assets/Scripts/PlayerAttacks.cs
+++ ludumdare52/Assets/Scripts/PlayerAttacks.cs
@@ -6,5 +6,34 @@ using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
-    
+    public Animator anim;
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask enemyLayer;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        //anim.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Hit " + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }
