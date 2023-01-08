a2d529493c0f414fd6e60c37f7169e590a81c291
Yaroslav <yarikmeisan@yandex.com>
2023-01-08 16:18:39 +0300

    Remove old system

--- ludumdare52/Assets/Scripts/PlayerAttacks.cs
+++ ludumdare52/Assets/Scripts/PlayerAttacks.cs
@@ -6,39 +6,5 @@ using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
-    public float timeBtwAttack;
-    public float startTimeBtwAttack;
-
-    public Transform attackPos;
-    public LayerMask whatIsEnemies;
-    public float attackRange;
-    public int damage;
-
-    private void Update()
-    {
-        if (timeBtwAttack <= 0)
-        {
-            if (Input.GetKey(KeyCode.L))
-            {
-                Collider2D[] enemiesToDamage =
-                    Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
-                }
-            }
-
-            timeBtwAttack = startTimeBtwAttack;
-        }
-        else
-        {
-            timeBtwAttack -= Time.deltaTime;
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
-    }
+    
 }
