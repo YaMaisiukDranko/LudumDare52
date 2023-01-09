d40f70c67bb8ba0b507848e8f1ac2b044d4bc94d
Yaroslav <yarikmeisan@yandex.com>
2023-01-09 08:30:56 +0300

    Create Player.cs

--- ludumdare52/Assets/Scripts/EnemyAttack.cs
+++ ludumdare52/Assets/Scripts/EnemyAttack.cs
@@ -1,14 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    public Animator anim;
     public float speed;
     public float stoppingDistance;
     private Transform target;
     
+    public float attackRate;
+    private float nextAttackTime;
+    private float attackRange;
+    public int damage;
+
+    public Transform attackPoint;
+    public LayerMask playerLayer;
+
+
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -20,5 +27,26 @@ public class EnemyAttack : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerBorder"))
+        {
+            if (Time.time >= nextAttackTime)
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //anim.SetTrigger("Attack");
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+        target.gameObject.GetComponent<Player>().TakeDamage(damage);
+        Debug.Log("Hit Player");
+        
     }
 }
